81b13253e696207275263b4a5b8502dfad3d6076
Mattias <tmatti3456@gmail.com>
2025-04-16 21:06:16 +0300

    latest

--- components/steam-cleaning-section.tsx
+++ components/steam-cleaning-section.tsx
@@ -40,7 +40,7 @@ const ServiceDetail = ({ title, description, icon, initiallyOpen = false }: Serv
           transition={{ duration: 0.3 }}
           className="p-4 bg-white dark:bg-gray-800"
         >
-          <p className="text-gray-700 dark:text-gray-300">{description}</
+          <p className="text-gray-700 dark:text-gray-300">{description}</p>
         </motion.div>
       )}
     </Card>
@@ -63,8 +63,7 @@ export function SteamCleaningSection() {
       <div
         className="absolute inset-0 opacity-5 dark:opacity-10 z-0"
         style={{
-          backgroundImage:
-            "url(\"data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fillRule='evenodd'%3E%3Cg fill='%231e40af' fillOpacity='0.2'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E\")",
+          backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fillRule='evenodd'%3E%3Cg fill='%231e40af' fillOpacity='0.2'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
         }}
       ></div>
 
